8d2c73ce0eef0f9ce3da66f49ec88e366c1c20ac
suu00k1459 <82146727+suu00k1459@users.noreply.github.com>
2024-04-19 10:28:03 +0900

    [level 2] Title: 짝지어 제거하기, Time: 135.60 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,16 +1,19 @@
+
+
 def solution(s):
-    lst = []
-    for i in s:
-        if len(lst) == 0:
-            lst.append(i)
-        else:
-            if i== lst[-1]:
-                lst.pop()
+    lst =list(s)
+    while True:
+        for i in range(0,len(lst)-1):
+            if lst[i]==lst[i+1]:
+                lst.pop(i+1)
+                lst.pop(i)
+                break
             else :
-                lst.append(i)
-    
-    
-    if len(lst) != 0:
-        return 0
-    else :
-        return 1
+                pass
+
+            if i==len(lst)-2:
+                return 0
+                
+        if len(lst)==0:
+            return 1
+        
