fad9e11ef6f402309ccf1aab7c8652b57428da23
suu00k1459 <82146727+suu00k1459@users.noreply.github.com>
2024-04-24 13:05:33 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,4 +1,5 @@
 import math
+
 def solution(progresses, speeds):
     answer = []
     a=0
@@ -6,22 +7,21 @@ def solution(progresses, speeds):
     day_list=[]
 
     for i in range(len(progresses)):
-        a= 100 - progresses[i] 
-        day = math.ceil(a/speeds[i])
-        day_list.append(day)
-        print("day",day)
+      a= 100 - progresses[i] 
+      day = a/speeds[i] #여기서 오류떴었음 -> 반례가 있었음, 처음에는 a//speeds[i]해서 몫만 구해졌던거
+      day = math.ceil(day)
+      day_list.append(day)
     print(day_list)
     
-    cnt = 1  #3
+    cnt = 1  
     max_day = day_list[0] 
     for j in range(1, len(day_list)):
         if day_list[j] <= max_day: 
             cnt += 1
         else:  
             answer.append(cnt)
-            print(cnt)
             cnt = 1
+            max_day = day_list[j]
             
-            max_day = day_list[j]   
     answer.append(cnt) 
     return answer
